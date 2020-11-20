7020e820b51a7cd4dc173fef1a1bf60ca7c239a7
Axel Schorcht <schorchtaxel@gmail.com>
2020-11-20 16:39:04 +0100

    Added all combined Plans roles

--- core/server/models/role.js
+++ core/server/models/role.js
@@ -75,9 +75,9 @@ Role = ghostBookshelf.Model.extend({
         if (action === 'assign' && loadedPermissions.user) {
             let checkAgainst;
             if (_.some(loadedPermissions.user.roles, {name: 'Owner'})) {
-                checkAgainst = ['Owner', 'Administrator', 'Editor', 'Author', 'Contributor', 'Planseditor'];
+                checkAgainst = ['Owner', 'Administrator', 'Editor', 'Author', 'Contributor', 'Planseditor', 'Planseditor Contributor', 'Planseditor Author', 'Planseditor Editor'];
             } else if (_.some(loadedPermissions.user.roles, {name: 'Administrator'})) {
-                checkAgainst = ['Administrator', 'Editor', 'Author', 'Contributor', 'Planseditor'];
+                checkAgainst = ['Administrator', 'Editor', 'Author', 'Contributor', 'Planseditor', 'Planseditor Contributor', 'Planseditor Author', 'Planseditor Editor'];
             } else if (_.some(loadedPermissions.user.roles, {name: 'Editor'})) {
                 checkAgainst = ['Author', 'Contributor'];
             }
