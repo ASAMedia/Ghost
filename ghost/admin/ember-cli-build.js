33ec315463cc340be2dc8875eef04e42fa76c67d
Axel <41484117+ASAMedia@users.noreply.github.com>
2024-11-18 01:48:10 +0100

    Updated dependencies so that the version still works without errors

--- ghost/admin/ember-cli-build.js
+++ ghost/admin/ember-cli-build.js
@@ -205,6 +205,7 @@ module.exports = function (defaults) {
         autoImport: {
             publicAssetURL,
             webpack: {
+                devtool: 'source-map',
                 resolve: {
                     fallback: {
                         util: require.resolve('util'),
@@ -212,6 +213,14 @@ module.exports = function (defaults) {
                         fs: false
                     }
                 },
+                ...(isDevelopment && {
+                    cache: {
+                        type: 'filesystem',
+                        buildDependencies: {
+                            config: [__filename]
+                        }
+                    }
+                }),
                 plugins: [
                     new webpack.ProvidePlugin({
                         process: 'process/browser'
